75c5b6ada9ef3055fd00482401b10089fa188a6f
Isabella Schvartz <isabellaschvartz@MacBook-Air-de-Isabella.local>
2026-07-31 16:19:07 -0300

    Recortar fotos de accesorios

--- script.js
+++ script.js
@@ -1236,7 +1236,7 @@ Object.entries(winterImageAdditionsByProductId).forEach(([productId, photoNumber
   ])].sort((a, b) => a - b);
 });
 
-const IMAGE_ASSET_VERSION = "20260731-8";
+const IMAGE_ASSET_VERSION = "20260731-9";
 
 function versionImageSrc(src) {
   return `${src}?v=${IMAGE_ASSET_VERSION}`;
