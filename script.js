97a482e3cda415328ac88af9a1882980bbce41df
Isabella Schvartz <isabellaschvartz@MacBook-Air-de-Isabella.local>
2026-08-04 13:14:52 -0300

    Actualizar colores del gorro acanalado

--- script.js
+++ script.js
@@ -1236,7 +1236,7 @@ Object.entries(winterImageAdditionsByProductId).forEach(([productId, photoNumber
   ])].sort((a, b) => a - b);
 });
 
-const IMAGE_ASSET_VERSION = "20260804-1";
+const IMAGE_ASSET_VERSION = "20260804-2";
 
 function versionImageSrc(src) {
   return `${src}?v=${IMAGE_ASSET_VERSION}`;
