43f486e203558b0c933f3a5c427ab315a3635503
Isabella Schvartz <isabellaschvartz@MacBook-Air-de-Isabella.local>
2026-08-13 12:55:56 -0300

    Corregir talles de nuevos productos de mujer

--- script.js
+++ script.js
@@ -2037,6 +2037,7 @@ products.push(...winterSourceProducts);
 const PRODUCTION_2027_REGULAR_SIZES = ["S", "M", "L", "XL", "XXL"];
 const PRODUCTION_2027_PLUS_SIZES = ["3XL", "4XL", "5XL", "6XL"];
 const PRODUCTION_2027_KIDS_SIZES = ["4", "6", "8", "10", "12", "14", "16"];
+const PRODUCTION_2027_LADIES_STOCK_SIZES = ["S", "M", "L", "XL"];
 
 function production2027Rows(color, curve, options = {}) {
   const sizes = options.sizes || PRODUCTION_2027_REGULAR_SIZES;
@@ -2353,17 +2354,17 @@ const production2027Products = [
     [172, "Aimmen", "125-627"], [173, "Licata", "225-052", "Cazadoras"], [174, "Sabina", "125-607"], [175, "Rennell", "126-312", "Cazadoras"], [176, "Hephzibah", "126-309", "Cazadoras"], [177, "Jules", "125-680", "Cazadoras"], [178, "Greta", "125-598", "Camperas de cuero"], [179, "Randit", "125-596", "Camperas de cuero"], [180, "Blyss", "225-553", "Cazadoras"], [181, "Maicyn", "126-306", "Cazadoras"], [182, "Simone", "125-594", "Cazadoras"], [183, "Peri", "125-599"], [184, "Gina", "125-367"], [185, "Magasa", "125-274"]
   ].map(([id, name, orderNumber, subcategory]) => production2027StockProduct({ id, name, orderNumber, category: "MUJER", subcategory, sourceWorkbook: "Stock Ladies MP088.pdf" })),
   ...[
-    [186, "Vetralia", "124-890", "Camperas", ["Camel", "Crudo"], PRODUCTION_2027_REGULAR_SIZES],
-    [187, "Cassola Fur", "125-448", "Camperas", ["Camel"], PRODUCTION_2027_REGULAR_SIZES],
-    [188, "Rayna", "126-310", "Camperas", ["Beige", "Militar"], PRODUCTION_2027_REGULAR_SIZES],
-    [189, "Holly", "126-134", "Cazadoras", ["Chocolate"], PRODUCTION_2027_REGULAR_SIZES],
-    [190, "Jabrielle", "126-307", "Camperas", ["Crudo"], PRODUCTION_2027_REGULAR_SIZES],
-    [191, "Keniya", "126-330", "Camperas", ["Negro"], PRODUCTION_2027_REGULAR_SIZES],
+    [186, "Vetralia", "124-890", "Camperas", ["Camel", "Crudo"], PRODUCTION_2027_LADIES_STOCK_SIZES],
+    [187, "Cassola Fur", "125-448", "Camperas", ["Camel"], PRODUCTION_2027_LADIES_STOCK_SIZES],
+    [188, "Rayna", "126-310", "Camperas", ["Beige", "Militar"], PRODUCTION_2027_LADIES_STOCK_SIZES],
+    [189, "Holly", "126-134", "Cazadoras", ["Chocolate"], PRODUCTION_2027_LADIES_STOCK_SIZES],
+    [190, "Jabrielle", "126-307", "Camperas", ["Crudo"], PRODUCTION_2027_LADIES_STOCK_SIZES],
+    [191, "Keniya", "126-330", "Camperas", ["Negro"], PRODUCTION_2027_LADIES_STOCK_SIZES],
     [192, "Lina Plus", "125-429", "Camperas", ["Crudo", "Gris", "Negro"], PRODUCTION_2027_PLUS_SIZES],
-    [193, "Aveleen", "226-024", "Cazadoras", ["Negro"], PRODUCTION_2027_REGULAR_SIZES],
-    [194, "Aliciana", "125-656", "Camperas", ["Camel", "Negro"], PRODUCTION_2027_REGULAR_SIZES],
-    [195, "Lesina", "125-164", "Camperas", ["Rosa"], PRODUCTION_2027_REGULAR_SIZES],
-    [196, "Nola", "225-260", "Camperas", ["Crudo"], PRODUCTION_2027_REGULAR_SIZES]
+    [193, "Aveleen", "226-024", "Cazadoras", ["Negro"], PRODUCTION_2027_LADIES_STOCK_SIZES],
+    [194, "Aliciana", "125-656", "Camperas", ["Camel", "Negro"], PRODUCTION_2027_LADIES_STOCK_SIZES],
+    [195, "Lesina", "125-164", "Camperas", ["Rosa"], PRODUCTION_2027_LADIES_STOCK_SIZES],
+    [196, "Nola", "225-260", "Camperas", ["Crudo"], PRODUCTION_2027_LADIES_STOCK_SIZES]
   ].map(([id, name, orderNumber, subcategory, colors, sizes]) => production2027StockProduct({
     id,
     name,
