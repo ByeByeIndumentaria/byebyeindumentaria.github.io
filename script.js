a6d9a3032ee73c77c0059dc5616d509ac720be1a
Isabella Schvartz <isabellaschvartz@MacBook-Air-de-Isabella.local>
2026-08-20 16:34:42 -0300

    Reemplazar fotos generales de Chomba Jersey Lisa Plus

--- script.js
+++ script.js
@@ -1396,7 +1396,7 @@ Object.entries(winterImageAdditionsByProductId).forEach(([productId, photoNumber
   ])].sort((a, b) => a - b);
 });
 
-const IMAGE_ASSET_VERSION = "20260820-50";
+const IMAGE_ASSET_VERSION = "20260820-51";
 
 const PRODUCTION_2027_IMAGE_SOURCE_BY_PRODUCT_ID = {
   118: 48,  // Merano
