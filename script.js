586dfbe52c5f2a57dabcb6d774d11b6796e2f87f
Isabella Schvartz <isabellaschvartz@MacBook-Air-de-Isabella.local>
2026-08-20 16:10:56 -0300

    Actualizar fotos por color de Bermuda lino

--- script.js
+++ script.js
@@ -1399,7 +1399,7 @@ Object.entries(winterImageAdditionsByProductId).forEach(([productId, photoNumber
   ])].sort((a, b) => a - b);
 });
 
-const IMAGE_ASSET_VERSION = "20260814-48";
+const IMAGE_ASSET_VERSION = "20260820-49";
 
 const PRODUCTION_2027_IMAGE_SOURCE_BY_PRODUCT_ID = {
   118: 48,  // Merano
